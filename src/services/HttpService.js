4eae6bf34e3ff3e0046844a232d23d45cd73df84
iddadda <iddadda241@gmail.com>
2025-06-25 13:20:09 +0900

    [feat]: memo 상세 페이지

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,16 +1,21 @@
-import axios from 'axios';
+// 통신담당
+import axios from "axios";
 
 export class HttpService {
-    addItem(item) {
-        axios.post('http://localhost:8080/memo', item)
-             .then(res => console.log('성공', res))
-             .catch(err => console.log('실패', err));
-    }
+  addItem(item) {
+    axios
+      .post("http://localhost:8080/memo", item)
+      .then((res) => console.log("성공", res))
+      .catch((err) => console.log("실패", err));
+  }
 
-    async getItems() {
-        const result = await axios.get('http://localhost:8080/memo');        
-        return result.data;
-    }
+  async getItems() {
+    const result = await axios.get("http://localhost:8080/memo");
+    return result.data;
+  }
 
-    
+  async getItem(id) {
+    const result = await axios.get(`http://localhost:8080/memo/${id}`);
+    return result.data;
+  }
 }
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,8 +1,9 @@
 <script setup>
 //p.175
-import { reactive, onMounted } from 'vue';
-import { HttpService } from '@/services/HttpService';
-import { useRoute, useRouter } from 'vue-router';
+// 메모등록, 메모 디테일, 메모 수정
+import { reactive, onMounted } from "vue";
+import { HttpService } from "@/services/HttpService";
+import { useRoute, useRouter } from "vue-router";
 
 const httpService = new HttpService();
 
@@ -10,41 +11,53 @@ const router = useRouter();
 const route = useRoute();
 
 const state = reactive({
-    memo: {
-        title: '',
-        content: ''
-    }
+  memo: {
+    id: 0,
+    title: "",
+    content: "",
+    createdAt: "",
+  },
 });
 
 const submit = () => {
-    httpService.addItem(state.memo);
-    alert('저장했습니다.');
-    router.push({ path: '/' });
-}
+  httpService.addItem(state.memo);
+  alert("저장했습니다.");
+  router.push({ path: "/" });
+};
 
-onMounted(()=> {
-    if(route.params.id) {//값이 있다면 item 클릭, 없다면 [+추가하기] 버튼 클릭
-        
-    }
+onMounted(async () => {
+  if (route.params.id) {
+    //값이 있다면 item 클릭, 없다면 [+추가하기] 버튼 클릭
+    state.memo = await httpService.getItem(route.params.id);
+    state.memo.id = parseInt(route.params.id);
+  }
 });
 </script>
 
 <template>
-    <form class="detail" @submit.prevent="submit">
-        <div class="mb-3">
-            <label for="title" class="form-label">제목</label>
-            <input type="text" id="title" class="form-control p-3" 
-                v-model="state.memo.title">                
-        </div>
-        <div class="mb-3">
-            <label for="content" class="form-label">내용</label>
-            <textarea id="content" class="form-control p-3"
-                v-model="state.memo.content"></textarea>             
-        </div>
-        <button class="btn btn-primary w-100 py-3">저장</button>
-    </form>
+  <form class="detail" @submit.prevent="submit">
+    <div class="mb-3" v-if="state.memo.createdAt">
+      등록일시: {{ state.memo.createdAt }}
+    </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">제목</label>
+      <input
+        type="text"
+        id="title"
+        class="form-control p-3"
+        v-model="state.memo.title"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="content" class="form-label">내용</label>
+      <textarea
+        id="content"
+        class="form-control p-3"
+        v-model="state.memo.content"
+      ></textarea>
+    </div>
+    <button class="btn btn-primary w-100 py-3">저장</button>
+  </form>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
